9b2f1e128ae73ec5dd4e3b784420b969c1e106ae
Angelina <ani-bri@ya.ru>
2023-04-04 11:54:03 +0300

    Updates

---
+++ precache-manifest.07fed4e94af9f2c883809c88e7c16437.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "340bfd423fd5c47484bc",
+    "url": "/portfolio/static/css/main.82404f87.chunk.css"
+  },
+  {
+    "revision": "340bfd423fd5c47484bc",
+    "url": "/portfolio/static/js/main.56b19fc5.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "2220bbf8abf0a72864d2",
+    "url": "/portfolio/static/js/2.13259073.chunk.js"
+  },
+  {
+    "revision": "2c395759fa7b4197cda5ed2c31a27340",
+    "url": "/portfolio/static/media/sun.2c395759.svg"
+  },
+  {
+    "revision": "3f19753b84db43e107296957a76ae147",
+    "url": "/portfolio/static/media/moon.3f19753b.svg"
+  },
+  {
+    "revision": "26cbd00266103d2e6105afb824553dc5",
+    "url": "/portfolio/static/media/vk.26cbd002.svg"
+  },
+  {
+    "revision": "0e14d3799c8f7d3329113145966fe6c3",
+    "url": "/portfolio/static/media/twitter.0e14d379.svg"
+  },
+  {
+    "revision": "391bb49ebad936293e3ab42042044956",
+    "url": "/portfolio/static/media/linkedIn.391bb49e.svg"
+  },
+  {
+    "revision": "dc7c8adc2a7508dd0034d1e2a02ae530",
+    "url": "/portfolio/static/media/instagram.dc7c8adc.svg"
+  },
+  {
+    "revision": "a31911e88fbccf0a879ee1a3709bd5b3",
+    "url": "/portfolio/static/media/gitHub.a31911e8.svg"
+  },
+  {
+    "revision": "6f3e199351c1a9e63fa72c93d9a8386f",
+    "url": "/portfolio/static/media/header-bg.6f3e1993.png"
+  },
+  {
+    "revision": "2229f8b2be373ee3d837e0c9330ec72f",
+    "url": "/portfolio/index.html"
+  }
+];
